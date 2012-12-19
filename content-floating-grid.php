50a56d20b6dd1a89cd885fc84609a129f7b2c37b
flashpatric <flashpatric@gmail.com>
2012-12-19 13:48:41 +0100

    Remove some debug stuff

--- content-floating-grid.php
+++ content-floating-grid.php
@@ -79,7 +79,7 @@ echo $subscriptionForm;
     </div>
     <?php endif; ?>
     <h2 class="entry-title grid-title"><a href="<?php the_permalink(); ?>" title="<?php printf(esc_attr__('Permalink to %s', 'themolio'), the_title_attribute('echo=0')); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-    <div class="entry-meta grid-meta">pop
+    <div class="entry-meta grid-meta">
         <?php themolio_posted_on(); ?>
         <?php if(comments_open() && ! post_password_required()) : ?>
         <?php comments_popup_link(__('Reply', 'themolio'), _x('1 Comment', 'comments number', 'themolio'), _x('% Comments', 'comments number', 'themolio'), 'entry-comments'); ?>
